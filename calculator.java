f50af36fdcbcef08c4d1113c8572069da882ceae
Priyanka Aggarwal <priyankaofficial1jan@gmail.com>
2019-01-17 10:42:31 +0530

    First commit master

---
+++ calculator.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class calculator{
+	public static void main(String[] args){
+		float a , b, res;
+		char choice , ch;
+		Scanner scan = new Scanner(System.in);
+		do{
+			System.out.println("1.Exit\n");
+			System.out.println("2.Add Two Numbers\n");
+			System.out.println("Enter your choice\n");
+			choice = scan.next().charAt(0);
+			switch(choice){
+				case '1': System.exit(0);
+					  break;
+				case '2': System.out.println("Enter 2 numbers\n");
+                                          a = scan.nextFloat();
+                                          b = scan.nextFloat();
+                                          res = a+b;
+                                          System.out.println("Result: "+res);
+                                          break;
+				default : System.out.println("Invalid choice!!\n");
+					  break;
+			
+			}
+			System.out.println();
+		}while(choice != 2);
+	}
+}
